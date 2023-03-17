84650c16c3a9f34f4e1f90ede4d7c104453252e7
quantumItInnovations <125892289+quantumItInnovations@users.noreply.github.com>
2023-03-17 12:56:28 +0530

    Update axiosUtil.js

--- src/axiosUtil.js
+++ src/axiosUtil.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
-  // baseURL: "https://boston-api.adaptable.app",
+  //baseURL: "http://localhost:5000",
+   baseURL: "https://boston-api.adaptable.app",
 });
 
 export default axiosInstance;
