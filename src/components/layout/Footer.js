79941d3d3905d7f12679b9bba4d06e71b956c5f7
Umesh Kumar <umesh98_soe@jnu.ac.in>
2023-03-17 18:25:24 +0530

    jwt expiration check

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,13 +5,14 @@ import { Store } from "../../Store";
 export default function Footer() {
   const { state, dispatch: ctxDispatch } = useContext(Store);
   const { userInfo } = state;
+  const d = new Date();
   return (
     <>
       {userInfo ? (
         <Container fluid className="p-0 mt-auto">
           <footer className="text-center">
             <strong>
-              Copyright © 2014-2023{" "}
+              Copyright © 2014-{`${d.getFullYear()} `}
               <a href="https://quantumitinnovation.com">Quantum It</a>.{" "}
             </strong>
             All rights reserved.
