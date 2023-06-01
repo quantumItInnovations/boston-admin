a5a8bc8fe8c4e61b7e33f547fbb480dec808bd0e
Umesh-JNU <umesh98_soe@jnu.ac.in>
2023-06-01 19:06:53 +0530

    update

--- src/utils/axiosUtil.js
+++ src/utils/axiosUtil.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   // baseURL: "http://52.91.135.217:5000",
-  baseURL: "http://localhost:5000",
-  // baseURL: "https://boston-api.adaptable.app",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://boston-api.adaptable.app",
   // baseURL: "https://api.bostongexotics.com",
 });
 
